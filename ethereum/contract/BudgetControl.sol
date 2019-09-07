5de5385b57cfb176b0dbb03a8f43bba380eac43b
ZhengxunWu <wuzhengxun@outlook.com>
2019-09-07 09:52:07 -0400

    contractUpdated and redployed

--- ethereum/contract/BudgetControl.sol
+++ ethereum/contract/BudgetControl.sol
@@ -6,9 +6,9 @@ contract CampaignFactory {
 
 
 
-    function createCampaign(uint minimum) public {
+    function createCampaign(uint minimum, uint goal, uint _healthcareMax, uint _educationMax, uint _InfraMax) public {
 
-        address newCampaign = new Campaign(minimum, msg.sender);
+        address newCampaign = new Campaign(minimum, goal, msg.sender,  _healthcareMax, _educationMax, _InfraMax);
 
         deployedCampaigns.push(newCampaign);
 
@@ -43,6 +43,8 @@ contract Campaign {
         mapping(address => bool) approvals;
 
     }
+    
+    
 
 
 
@@ -51,6 +53,14 @@ contract Campaign {
     address public manager;
 
     uint public minimumContribution;
+    
+    uint public totalBudget;
+    
+    uint public healthcareMax;
+    
+    uint public educationMax;
+    
+    uint public InfraMax;
 
     mapping(address => bool) public approvers;
 
@@ -67,13 +77,21 @@ contract Campaign {
     }
 
 
-
-    function Campaign(uint minimum, address creator) public {
+   //government make an intiative for some public goods
+    function Campaign(uint minimum, uint goal, address creator, uint _healthcareMax, uint _educationMax, uint _InfraMax) public {
 
         manager = creator;
 
         minimumContribution = minimum;
-
+        
+        totalBudget = goal;
+        
+        healthcareMax=_healthcareMax;
+        
+        educationMax=_educationMax;
+        
+        InfraMax=_InfraMax;
+        
     }
 
 
@@ -83,17 +101,33 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
 
-
         approvers[msg.sender] = true;
 
         approversCount++;
 
     }
 
+//      function compareStrings (string memory a, string memory b) public view 
+//       returns (bool) {
+//   return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))) );
 
+//       }
 
-    function createRequest(string description, uint value, address recipient) public adminOnly {
+    // 0:healthcare 1: education 2: infrastructure
 
+    function createRequest(string description, uint sector, uint value, address recipient) public adminOnly {
+        
+        if (sector==0) {
+            require(value<=healthcareMax);
+        }
+        if(sector==1) {
+            require(value<=educationMax);
+        }
+        if(sector==2){
+            require(value<=InfraMax);
+        }
+        
+        
         Request memory newRequest = Request({
 
            description: description,
@@ -140,14 +174,14 @@ contract Campaign {
 
         Request storage request = requests[index];
 
-
-
+       
+       //must exceed half of the citizens
         require(request.approvalCount > (approversCount / 2));
-
+       //prevent double spending
         require(!request.complete);
 
 
-
+       //execute the money transfer automatically and immediately as long as the requirement has been met 
         request.recipient.transfer(request.value);
 
         request.complete = true;
