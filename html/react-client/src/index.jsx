0e4b35a64ff521d492e5f55e9e360fe4e5892a06
Sreya Das <1sreyadas@gmail.com>
2019-09-07 07:13:51 -0400

    react html

---
+++ html/react-client/src/index.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+import factory from '../ethereum/factory';
+ReactDOM.render(<App />, document.getElementById('app'));
